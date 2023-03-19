fd50fc58c0d14cad169b4deb2695d3d662a5d8f7
Ruvin Rodrigues <ruvin27@gmail.com>
2023-03-19 00:34:30 -0500

    minor changes

--- app/src/main/java/com/uta/expensetracker/MainActivity.java
+++ app/src/main/java/com/uta/expensetracker/MainActivity.java
@@ -3,11 +3,8 @@ package com.uta.expensetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         btnLogOut = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
