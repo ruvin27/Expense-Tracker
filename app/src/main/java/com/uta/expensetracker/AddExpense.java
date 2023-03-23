681b39c06e4f835522f54bab9dfc3e49de24ee58
ashmitha <ashmitha.rs@gmail.com>
2023-03-23 16:22:28 -0500

    Added profile, edit profile,add expense design pages. Also added add expense activity page. Changed all of the pages to the correct design.

---
+++ app/src/main/java/com/uta/expensetracker/AddExpense.java
@@ -0,0 +1,188 @@
+package com.uta.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class AddExpense extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+
+        spinner = findViewById(R.id.spinner);
+
+        List<String> categories = new ArrayList<>();
+        categories.add(0,"choose category");
+        categories.add("Food");
+        categories.add("Gorcery");
+        categories.add("Rent");
+        categories.add("MISC");
+
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(parent.getItemAtPosition(position).equals("choose category")){
+
+                }
+
+                else{
+                    String item = parent.getItemAtPosition(position).toString();
+
+                    Toast.makeText(parent.getContext(), "Selected:" +item , Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+//    ArrayAdapter<String> arrayAdapter;
+////    String[] Category = {0,"choose category"};
+//    String[] Category = { "Food", "Gorcery", "Rent", "MISC"};
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        initDatepicker();
+//        dateButton= findViewById(R.id.datepickerButton);
+//        dateButton.setText(getTodaysDate());
+//
+//        spinner = findViewById(R.id.spinner);
+//        arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,Category);
+//        spinner.setAdapter(arrayAdapter);
+//
+//
+//       spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//           @Override
+//           public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+//
+//           }
+//
+//           @Override
+//           public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//           }
+//       });
+//
+//
+//    }
+
+
+
+
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month= month+ 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDareString(day, month, year);
+
+    }
+
+    private void initDatepicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener= new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month +1;
+                String date = makeDareString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
+
+    }
+
+    private String makeDareString(int day, int month, int year) {
+        return getMonthFormat(month)+ " "+day+" "+year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month == 1)
+            return "JAN";
+
+        if (month == 2)
+            return "FEB";
+
+        if (month == 3)
+            return "MAR";
+
+        if (month == 4)
+            return "APR";
+
+        if (month == 5)
+            return "MAY";
+
+        if (month == 6)
+            return "JUN";
+
+        if (month == 7)
+            return "JUL";
+
+        if (month == 8)
+            return "AUG";
+
+        if (month == 9)
+            return "SEP";
+
+        if (month == 10)
+            return "OCT";
+
+        if (month == 11)
+            return "NOV";
+
+        if (month == 12)
+            return "DEC";
+
+        return "JAN";
+
+    }
+
+
+    public void openDatepicker(View view) {
+        datePickerDialog.show();
+    }
+}
