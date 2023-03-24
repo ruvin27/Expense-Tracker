c73b37fb121ab664d63f2dd306e4864f6a081e8f
ashmitha <ashmitha.rs@gmail.com>
2023-03-24 10:49:17 -0500

    Added deleteUpdateExpense activity and the xml file. Cleared bug in the AddExpense activity(date was not working).

--- app/src/main/java/com/uta/expensetracker/DeleteUpdateExpense.java
+++ app/src/main/java/com/uta/expensetracker/DeleteUpdateExpense.java
@@ -2,8 +2,12 @@ package com.uta.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -17,7 +21,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class AddExpense extends AppCompatActivity {
+public class DeleteUpdateExpense extends AppCompatActivity {
+
 
     private DatePickerDialog datePickerDialog;
     private Button dateButton;
@@ -25,26 +30,27 @@ public class AddExpense extends AppCompatActivity {
 
     private Spinner spinner;
 
-    @Override
-    protected void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_expense);
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_update_expense);
         initDatepicker();
         dateButton = findViewById(R.id.datepickerButton);
         dateButton.setText(getTodaysDate());
+
         spinner = findViewById(R.id.spinner);
 
         List<String> categories = new ArrayList<>();
-        categories.add(0,"choose");
+        categories.add(0, "choose");
         categories.add("Food");
-        categories.add("Grocery");
+        categories.add("Groceries");
         categories.add("Rent");
         categories.add("MISC");
 
 
         ArrayAdapter<String> dataAdapter;
-        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,categories);
+        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(dataAdapter);
         spinner.setSelection(0,false);
@@ -53,14 +59,12 @@ public class AddExpense extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                if(parent.getItemAtPosition(position).equals("choose")){
+                if (parent.getItemAtPosition(position).equals("choose")) {
 
-                }
-
-                else{
+                } else {
                     String item = parent.getItemAtPosition(position).toString();
 
-                    Toast.makeText(parent.getContext(), "Selected:" +item , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(parent.getContext(), "Selected:" + item, Toast.LENGTH_SHORT).show();
 
                 }
             }
@@ -71,60 +75,24 @@ public class AddExpense extends AppCompatActivity {
             }
         });
 
-
     }
-//    ArrayAdapter<String> arrayAdapter;
-////    String[] Category = {0,"choose category"};
-//    String[] Category = { "Food", "Gorcery", "Rent", "MISC"};
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        initDatepicker();
-//        dateButton= findViewById(R.id.datepickerButton);
-//        dateButton.setText(getTodaysDate());
-//
-//        spinner = findViewById(R.id.spinner);
-//        arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,Category);
-//        spinner.setAdapter(arrayAdapter);
-//
-//
-//       spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//           @Override
-//           public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-//
-//           }
-//
-//           @Override
-//           public void onNothingSelected(AdapterView<?> adapterView) {
-//
-//           }
-//       });
-//
-//
-//    }
-
-
-
 
 
     private String getTodaysDate() {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
-        month= month+ 1;
+        month = month + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         return makeDateString(day, month, year);
 
     }
 
     private void initDatepicker() {
-        DatePickerDialog.OnDateSetListener dateSetListener= new DatePickerDialog.OnDateSetListener() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month +1;
+                month = month + 1;
                 String date = makeDateString(day, month, year);
                 dateButton.setText(date);
             }
@@ -137,13 +105,13 @@ public class AddExpense extends AppCompatActivity {
 
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
-
-        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
         datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
     }
 
     private String makeDateString(int day, int month, int year) {
-        return getMonthFormat(month)+ " "+day+" "+year;
+        return getMonthFormat(month) + " " + day + " " + year;
     }
 
     private String getMonthFormat(int month) {
