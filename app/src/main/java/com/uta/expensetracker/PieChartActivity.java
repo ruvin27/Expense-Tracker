f03aabe8b19362b815e701bd9cbd9c965d31725c
ashmitha <ashmitha.rs@gmail.com>
2023-04-16 12:10:44 -0500

    Changed Pie chart.

--- app/src/main/java/com/uta/expensetracker/PieChartActivity.java
+++ app/src/main/java/com/uta/expensetracker/PieChartActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -21,6 +25,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -44,47 +49,51 @@ public class PieChartActivity extends AppCompatActivity {
         expenseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DecimalFormat df = new DecimalFormat("0.00");
                 HashMap<String, Float> Category = new HashMap<String, Float>();
 
                 // Iterate through the DataSnapshot objects to extract the expenses
                 if (snapshot.exists() && snapshot.hasChildren()) {
                     for (DataSnapshot expenseSnapshot : snapshot.getChildren()) {
                         Expense expense = expenseSnapshot.getValue(Expense.class);
-                        if (Category.containsKey(expense.category)){
+                        if (Category.containsKey(expense.category)) {
                             Float amt = Category.get(expense.category);
-                            Category.put(expense.category, new Float(expense.amount+amt));
-                        }
-                        else{
+                            Category.put(expense.category, new Float(expense.amount + amt));
+                        } else {
                             Category.put(expense.category, new Float(expense.amount));
                         }
                         System.out.println(Category);
                     }
-                }
+                    ArrayList<PieEntry> entries = new ArrayList<>();
+                    entries.add(new PieEntry(Category.get("Food"), "Food"));
+                    entries.add(new PieEntry(Category.get("Rent"), "Rent"));
+                    entries.add(new PieEntry(Category.get("Grocery"), "Grocery"));
+                    entries.add(new PieEntry(Category.get("MISC"), "MISC"));
 
+
+                    PieDataSet pieDataSet = new PieDataSet(entries, "Expenses by Category");
+                    pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                    pieDataSet.setValueTextSize(20f);
+                    PieData data = new PieData(pieDataSet);
+
+                    pieChart.setData(data);
+                    pieChart.getDescription().setEnabled(false);
+
+                    pieChart.setCenterText("Categories");
+                    pieChart.setCenterTextSize(22f);
+                    pieChart.animateY(2000, Easing.EaseInOutQuad);
+                    pieChart.invalidate();
+
+
+
+                }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Handle database error
+                Log.d("Firebase", error.getMessage());
             }
         });
 
-
-        ArrayList<PieEntry> categories = new ArrayList<>();
-        categories.add(new PieEntry(500,"Food"));
-        categories.add(new PieEntry(500,"Grocery"));
-        categories.add(new PieEntry(500,"Misc"));
-        categories.add(new PieEntry(500,"Rent"));
-
-        PieDataSet pieDataSet = new PieDataSet(categories, "Categories");
-        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        pieDataSet.setValueTextColor(Color.BLACK);
-        pieDataSet.setValueTextSize(16f);
-        PieData pieData = new PieData((pieDataSet));
-
-        pieChart.setData(pieData);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.setCenterText("Categories");
-        pieChart.setCenterTextSize(22f);
-        pieChart.animate();
     }
 }
